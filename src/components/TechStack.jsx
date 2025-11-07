d8c4ec97ac0055773a95e86f56a1d72b8900a224
rcd-env <donerakesh001@gmail.com>
2025-11-08 01:38:38 +0530

    fix: update Arsenal label styling for improved readability in TechStack component

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -30,7 +30,7 @@ function TechStack() {
     <div className="h-full p-6 border rounded-lg cursor-grab">
       <div className="flex gap-4 mb-4 text-7xl items-center">
         <Braces size={54} />{" "}
-        <span className="text-2xl md:text-3xl font-bold ">Arsenal</span>
+        <span className="text-2xl font-bold tracking-widest">Arsenal</span>
       </div>
       <hr />
       {frontend.length > 0 && (
