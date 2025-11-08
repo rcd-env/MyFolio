2360e0bc63aba2249756c70570e93838c8720277
rcd-env <donerakesh001@gmail.com>
2025-11-09 01:35:55 +0530

    refactor: update Quote component layout and image source for consistency

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,15 +2,9 @@ import React from "react";
 
 function Quote() {
   return (
-    <div className="w-full flex flex-col items-center gap-1 mt-auto cursor-grab">
-      <img
-        src="/images/hire-me.jpeg"
-        alt=""
-        className="h-full w-full rounded-lg"
-      />
-      <p className="mr-auto text-lg italic flex items-center justify-center">
-        ~ “Hire me, Please.”
-      </p>
+    <div className="w-full flex flex-col gap-1 mt-auto cursor-grab">
+      <img src="/images/shy.jpg" alt="" className="h-full w-full rounded-lg" />
+      <p className="mr-auto md:ml-auto text-lg italic">~ Hire me, Please.</p>
     </div>
   );
 }
