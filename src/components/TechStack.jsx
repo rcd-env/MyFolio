e51400386642992936a502dc12a66f70107603e1
rcd-env <donerakesh001@gmail.com>
2025-11-09 16:13:23 +0530

    style: adjust spacing and font sizes in TechStack component for improved layout

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -28,17 +28,20 @@ function TechStack() {
 
   return (
     <div className="h-full p-6 border rounded-lg cursor-grab">
-      <div className="flex gap-4 mb-4 text-7xl items-center">
+      <div className="flex gap-4 mb-2 text-7xl items-center">
         <Braces size={54} />{" "}
         <span className="text-2xl font-bold tracking-widest">Arsenal</span>
       </div>
       <hr />
       {frontend.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">Frontend</h2>
+        <div className="mt-2">
+          <h2 className="text-lg font-semibold mb-2">Frontend</h2>
           <div className="flex flex-wrap">
             {frontend.map((tech) => (
-              <div key={tech} className="mr-3">
+              <div
+                key={tech}
+                className="text-xs mr-2 mb-1 py-1 px-2 border rounded-full"
+              >
                 {tech}
               </div>
             ))}
@@ -46,11 +49,14 @@ function TechStack() {
         </div>
       )}
       {backend.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">Backend</h2>
+        <div className="mt-1">
+          <h2 className="text-lg font-semibold mb-2">Backend</h2>
           <div className="flex flex-wrap">
             {backend.map((tech) => (
-              <div key={tech} className="mr-3">
+              <div
+                key={tech}
+                className="text-xs mr-2 mb-1 py-1 px-2 border rounded-full"
+              >
                 {tech}
               </div>
             ))}
@@ -58,11 +64,14 @@ function TechStack() {
         </div>
       )}
       {database.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">Database</h2>
+        <div className="mt-1">
+          <h2 className="text-lg font-semibold mb-2">Database</h2>
           <div className="flex flex-wrap">
             {database.map((tech) => (
-              <div key={tech} className="mr-3">
+              <div
+                key={tech}
+                className="text-xs mr-2 mb-1 py-1 px-2 border rounded-full"
+              >
                 {tech}
               </div>
             ))}
@@ -70,11 +79,14 @@ function TechStack() {
         </div>
       )}
       {language.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">Languages</h2>
+        <div className="mt-1">
+          <h2 className="text-lg font-semibold mb-2">Languages</h2>
           <div className="flex flex-wrap">
             {language.map((tech) => (
-              <div key={tech} className="mr-3">
+              <div
+                key={tech}
+                className="text-xs mr-2 mb-1 py-1 px-2 border rounded-full"
+              >
                 {tech}
               </div>
             ))}
@@ -82,11 +94,14 @@ function TechStack() {
         </div>
       )}
       {extra.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">Extras</h2>
+        <div className="mt-1">
+          <h2 className="text-lg font-semibold mb-2">Extras</h2>
           <div className="flex flex-wrap">
             {extra.map((tech) => (
-              <div key={tech} className="mr-3">
+              <div
+                key={tech}
+                className="text-xs mr-2 mb-1 py-1 px-2 border rounded-full"
+              >
                 {tech}
               </div>
             ))}
